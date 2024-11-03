58893535bb1fc521127bd882bb66b2741a75a83f
天仙婆婆 <tqx5201@163.com>
2024-11-03 19:42:07 +0800

    Update $userId.tsx

--- app/routes/$userId.tsx
+++ app/routes/$userId.tsx
@@ -1,9 +1,7 @@
 // app/routes/user/$id.jsx
-import { json } from '@remix-run/node';
-
 export async function loader({ params }) {
   const userId = params.id;
 
   // 现在你可以使用 userId 变量了
-  return json({ userId });
+  return userId;
 }
