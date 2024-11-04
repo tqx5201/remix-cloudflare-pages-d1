b9044dd813ee75068a32b6c23d15327db222b573
天仙婆婆 <tqx5201@163.com>
2024-11-04 17:10:36 +0800

    Update sxg.$id.tsx

--- app/routes/sxg.$id.tsx
+++ app/routes/sxg.$id.tsx
@@ -27,7 +27,9 @@ for (var i = 0; i < 6; i++) {
     //header("Content-Disposition: attachment; filename=mnf.m3u8");
     //console.log(current);
     return new Response(current, {
-    headers: { 'Content-Type': 'application/vnd.apple.mpegurl' },
+    headers: { 'Content-Type': 'application/vnd.apple.mpegurl',
+	       'Content-Disposition': 'attachment; filename=mnf.m3u8'
+	     },
   });
 
 }
