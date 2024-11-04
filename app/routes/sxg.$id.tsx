01cef84739e7b1e613cb795c9278acf4fcb13803
天仙婆婆 <tqx5201@163.com>
2024-11-04 22:19:27 +0800

    Update sxg.$id.tsx

--- app/routes/sxg.$id.tsx
+++ app/routes/sxg.$id.tsx
@@ -1,21 +1,20 @@
 //import{ useLoaderData } from "@remix-run/react";
 
 export const loader = async ({ request, params }: LoaderFunctionArgs) => {
-const domain = 'http://live2.rxip.sc96655.com';
-//const [searchParams] = useSearchParams();
-//const playseek = searchParams.get("playseek");
-const url = new URL(request.url);
-const playseek = url.searchParams.get("playseek");
-const { id } = params;//另一种写法params.id;//'emdy_800';
-if(playseek){
-    get_back(playseek,id,domain);
-}else{
-	get_live(id,domain);
-}
-	
-	
-
-
+	const domain = 'http://live2.rxip.sc96655.com';
+	//const [searchParams] = useSearchParams();
+	//const playseek = searchParams.get("playseek");
+	const url = new URL(request.url);
+	const playseek = url.searchParams.get("playseek");
+	const { id } = params;//另一种写法params.id;//'emdy_800';
+	if(playseek){
+	    get_back(playseek,id,domain);
+	}else{
+		let current = get_live(id,domain);
+		return new Response(current, {
+			headers: { 'Content-Type': 'text/plain'},
+		});
+	}
 }
 //export async function loader() {}
 function get_live(ids,domain){
@@ -26,7 +25,7 @@ function get_live(ids,domain){
 	let ch_qxd = ch_arr[1];
 		
 	let stream = `${domain}/live/8ne5i_sccn,${ch_id}_hls_pull_${ch_qxd}K/`;
-	var time=Math.floor(new Date().getTime()/1000);
+	var time = Math.floor(new Date().getTime()/1000);
 	let timestamp = parseInt((time-60)/6);
 	let current = "#EXTM3U\r\n";
 	current += "#EXT-X-VERSION:3\r\n";
@@ -44,7 +43,5 @@ function get_live(ids,domain){
 	//header('Content-Type: application/vnd.apple.mpegurl');
 	//header("Content-Disposition: attachment; filename=mnf.m3u8");
 	//console.log(current);
-	return new Response(current, {
-		headers: { 'Content-Type': 'application/vnd.apple.mpegurl'},
-	});
+	return current;
 }
