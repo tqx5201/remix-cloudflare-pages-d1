c275113b1b2b64f3527f2b7cf1ed020a4e050f5d
天仙婆婆 <tqx5201@163.com>
2024-11-06 10:00:01 +0800

    Update 1905.$id.tsx

--- app/routes/1905.$id.tsx
+++ app/routes/1905.$id.tsx
@@ -28,7 +28,7 @@ export const loader = async ({ request, params }: LoaderFunctionArgs) => {
     const hashBuffer = await crypto.subtle.digest('SHA-1', new TextEncoder().encode(http_query));
     const hashArray = Array.from(new Uint8Array(hashBuffer));
     const sign = hashArray.map(b =>b.toString(16).padStart(2, '0')).join('');
-
+return new Response(sign);
     myParams['appid'] = 'W0hUwz8D';
 
     const originalUrl = "https://profile.m1905.com/mvod/liveinfo.php";
