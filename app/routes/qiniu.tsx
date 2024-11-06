4b8f17ec45c8b5ebc197b21e3aef80956ecac57e
天仙婆婆 <tqx5201@163.com>
2024-11-06 22:26:37 +0800

    Update qiniu.tsx

--- app/routes/qiniu.tsx
+++ app/routes/qiniu.tsx
@@ -1,10 +1,10 @@
 //import{ useLoaderData } from "@remix-run/react";
 
 export const loader = async ({ params }: LoaderFunctionArgs) => {
-
+  return new Response(process.env);
   // 替换为你的七牛云 Access Key 和 Secret Key
   const ACCESS_KEY = '_xrjdAPeGkNOy_Kuw_ZK-CarsgrnJbuGMyt5gxZP'
-  return new Response(ACCESS_KEY);
+  return new Response(process.env);
   const SECRET_KEY = process.env.QINIU_SECRET_KEY;  //'QFOnGvqUDF4EGO3n02ybiSdKjKAjWMk7gAHEcWkG'
 
   // 替换为你的七牛云存储空间名称和文件名称
