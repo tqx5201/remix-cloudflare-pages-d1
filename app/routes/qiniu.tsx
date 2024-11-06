f067819cb90b9e6217c983294285f757723c1e3d
天仙婆婆 <tqx5201@163.com>
2024-11-06 21:45:16 +0800

    Update qiniu.tsx

--- app/routes/qiniu.tsx
+++ app/routes/qiniu.tsx
@@ -35,7 +35,7 @@ export const loader = async ({ request, params }: LoaderFunctionArgs) => {
 //return new Response(uploadToken);
 
   // 构建上传请求
-  const uploadUrl = `https://upload.qiniup.com/putb64/-1/key/${btoa(FILE_NAME)}`
+  const uploadUrl = `https://upload.qiniup.com/putb64/-2/key/${btoa(FILE_NAME)}`
   //const uploadUrl = 'http://up-z2.qiniup.com'	
   const uploadRequest = new Request(uploadUrl, {
     method: 'POST',
