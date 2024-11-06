f1a66f72e7934aac371c7ae7ee6b7c6fa54f4d78
天仙婆婆 <tqx5201@163.com>
2024-11-06 17:32:36 +0800

    Update list.$id.tsx

--- app/routes/list.$id.tsx
+++ app/routes/list.$id.tsx
@@ -1,7 +1,7 @@
 
 export const loader = async ({ context,params }: LoaderFunctionArgs) => {
   const db = context.DB as D1Database;
-  const { id } = params.replace('.txt',''); 
+  const { id } = params; 
   const { results }= await db
     .prepare("SELECT * FROM iptv_list where yys = ?")
     .bind(id)
