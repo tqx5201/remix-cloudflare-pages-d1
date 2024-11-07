aa1cd44eeee6a29dab445e2b748aead0878e329a
天仙婆婆 <tqx5201@163.com>
2024-11-07 16:45:45 +0800

    Update remix.env.d.ts

--- remix.env.d.ts
+++ remix.env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="@remix-run/dev" />
 /// <reference types="@remix-run/cloudflare" />
 /// <reference types="@cloudflare/workers-types" />
-interface Env extends z.infer<typeof serverSchema> {
+interface Env {
     QINIU_ACCESS_KEY: string;
     QINIU_SECRET_KEY: string;
   }
