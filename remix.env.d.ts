e0b87a9ea8884a6e332790fabc37f67d10730341
天仙婆婆 <tqx5201@163.com>
2024-11-07 16:48:03 +0800

    Update remix.env.d.ts

--- remix.env.d.ts
+++ remix.env.d.ts
@@ -5,3 +5,21 @@ interface Env {
     QINIU_ACCESS_KEY: string;
     QINIU_SECRET_KEY: string;
   }
+/// <reference types="remix-run/server" />
+
+
+declare namespace NodeJS {
+  interface ProcessEnv extends Env {}
+}
+
+declare module '@remix-run/node' {
+  interface PlatformContext {
+    env: Env;
+  }
+}
+
+declare module '@remix-run/cloudflare' {
+  interface PlatformContext {
+    env: Env;
+  }
+}
