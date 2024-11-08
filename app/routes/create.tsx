6e60b23400cf7c148d289cada2ace886001d08a2
天仙婆婆 <tqx5201@163.com>
2024-11-08 13:09:09 +0800

    Update create.tsx

--- app/routes/create.tsx
+++ app/routes/create.tsx
@@ -8,10 +8,10 @@ export const loader = async ({ context }: LoaderArgs) => {
   //const { results } = await db
     //.prepare(sql)
     //.all<iptv_list>();
-  await db.exec(`CREATE TABLE IF NOT EXISTS users (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT NOT NULL,
-  email TEXT UNIQUE NOT NULL)`);
+  //await db.exec(`CREATE TABLE IF NOT EXISTS users (
+  //id INTEGER PRIMARY KEY AUTOINCREMENT,
+  //name TEXT NOT NULL,
+  //email TEXT UNIQUE NOT NULL)`);
   return new Response('results');
   return json({
     iptv_lists: results ?? [],
