ab87581ff54f7e15403c09081c2f04c699ffbf10
天仙婆婆 <tqx5201@163.com>
2024-11-08 12:36:23 +0800

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -17,7 +17,7 @@ type Customer = {
 
 export const loader = async ({ context }: LoaderArgs) => {
   const db = context.DB as D1Database;
-  const sql = 'CREATE TABLE Customers222 (CustomerID INT PRIMARY KEY autoincrement, CompanyName TEXT, ContactName TEXT, PRIMARY KEY (`CustomerID`));;
+  const sql = 'CREATE TABLE Customers222 (CustomerID INT PRIMARY KEY autoincrement, CompanyName TEXT, ContactName TEXT, PRIMARY KEY (`CustomerID`));';
   const { results } = await db
     .prepare(sql);
     //.all<iptv_list>();
