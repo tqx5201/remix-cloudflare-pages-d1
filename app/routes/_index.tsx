90a2a840f1138b672ff829accee1c06f6a612daa
天仙婆婆 <tqx5201@163.com>
2024-11-08 12:42:11 +0800

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -19,9 +19,10 @@ export const loader = async ({ context }: LoaderArgs) => {
   const db = context.DB as D1Database;
   const sql = 'CREATE TABLE Customers222 (CustomerID INT PRIMARY KEY autoincrement, CompanyName TEXT, ContactName TEXT, PRIMARY KEY (`CustomerID`));';
   const { results } = await db
-    .prepare(sql);
+    //.prepare(sql);
     //.all<iptv_list>();
-
+    .exec(sql);
+  return new Response(results);
   return json({
     iptv_lists: results ?? [],
   });
