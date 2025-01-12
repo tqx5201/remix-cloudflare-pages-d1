79b9e3be875bf039b32d0f384a27df8cbfb8a850
天仙婆婆 <tqx5201@163.com>
2025-01-12 11:41:35 +0800

    Update qiniu.$id.tsx

--- app/routes/qiniu.$id.tsx
+++ app/routes/qiniu.$id.tsx
@@ -1,4 +1,3 @@
-//import{ useLoaderData } from "@remix-run/react";
 
 export const loader = async ({ context,params }: LoaderFunctionArgs) => {
   const db = context.DB as D1Database;
@@ -14,66 +13,8 @@ export const loader = async ({ context,params }: LoaderFunctionArgs) => {
       re_str += mergeItems(obj.list) + '\n';
   }
   return new Response(re_str);
-}
+};
 
-function up2qiniu(up_str,id){
-  //将上面整理好的代码上传至qiniu
-  // 替换为你的七牛云 Access Key 和 Secret Key
-  const ACCESS_KEY = '_xrjdAPeGkNOy_Kuw_ZK-CarsgrnJbuGMyt5gxZP'
-  //return new Response(process.env.QINIU_ACCESS_KEY);
-  const SECRET_KEY = 'QFOnGvqUDF4EGO3n02ybiSdKjKAjWMk7gAHEcWkG'
-
-  // 替换为你的七牛云存储空间名称和文件名称
-  const BUCKET_NAME = 'diyp'
-  const FILE_NAME = 'list_' + id + '_cf.txt'
-
-  // 要上传的字符串
-  //const stringToUpload = 'Hello, Qiniu Cloud!'
-  const stringToUpload = up_str
-
-  // 构建上传策略
-  const policy = {
-    scope: `${BUCKET_NAME}:${FILE_NAME}`,
-    deadline: Math.floor(Date.now() / 1000) + 3600 // 1小时有效期
-  }
-
-  // 使用 HMAC-SHA1 签名策略
-  const encodedPolicy = btoa(JSON.stringify(policy))
-  const sign = await crypto.subtle.importKey(
-    'raw',
-    new TextEncoder().encode(SECRET_KEY),
-    { name: 'HMAC', hash: 'SHA-1' },
-    false,
-    ['sign']
-  ).then(key => crypto.subtle.sign('HMAC', key, new TextEncoder().encode(encodedPolicy)))
-    .then(arrayBuffer => btoa(String.fromCharCode(...new Uint8Array(arrayBuffer))))
-
-  // 构建上传凭证
-  const uploadToken = `${ACCESS_KEY}:${sign}:${encodedPolicy}`
-//return new Response(uploadToken);
-
-  // 构建上传请求
-  //const uploadUrl = `https://upload.qiniup.com/putb64/-2/key/${btoa(FILE_NAME)}`
-  const uploadUrl = `http://up-z2.qiniup.com/putb64/-1/key/${btoa(FILE_NAME)}`
-  const uploadRequest = new Request(uploadUrl, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/octet-stream',
-      'Authorization': `UpToken ${uploadToken}`
-    },
-    body: btoa(stringToUpload)
-  })
-
-  // 发送上传请求
-  const response = await fetch(uploadRequest);
-
-  // 返回响应结果
-  return response;
-}
-
-
-
-//合并列表
 function mergeItems(str) {
     let arr = str.split('\n');
     // 过滤掉空值
