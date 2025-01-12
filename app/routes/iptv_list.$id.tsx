0f6754553b7425737c852a3073ee7928490246fa
天仙婆婆 <tqx5201@163.com>
2025-01-12 13:29:44 +0800

    Update iptv_list.$id.tsx

--- app/routes/iptv_list.$id.tsx
+++ app/routes/iptv_list.$id.tsx
@@ -1,30 +1,9 @@
-// app/routes/form-example.jsx
-
-import type { LoaderArgs } from "@remix-run/cloudflare";
 import { json } from "@remix-run/cloudflare";
-import { useLoaderData } from "@remix-run/react";
+import { useActionData } from "@remix-run/react";
 
+export async function action({ request }) {
+  const formData = await request.formData();
+  const name = formData.get("name");
 
-export const loader = async ({ context,params }: LoaderFunctionArgs) => {
-
-// 将对象转换为JSON字符串
-  const jsonString = JSON.stringify(params);
-  // 返回JSON响应
-  return json({ jsonString });
-
-  
-  return new Response(params);
-  const db = context.DB as D1Database;
-  const { id } = params; 
-  const ids = id.split('.')[0];
-  const { results } = await db
-    .prepare("SELECT * FROM iptv_list where yys = ?")
-    .bind(ids)
-    .all();
-  let re_str = '';
-  for (const obj of results) {
-      re_str += obj.name + ',#genre#\n';
-      re_str += mergeItems(obj.list) + '\n';
-  }
-  return new Response(re_str);
-};
+  return json({ message: `Hello, ${name}!` });
+}
