c00db00cc1e5862c7bb627063680ecd4bbcf5596
天仙婆婆 <tqx5201@163.com>
2025-01-12 11:51:11 +0800

    Update qiniu.$id.tsx

--- app/routes/qiniu.$id.tsx
+++ app/routes/qiniu.$id.tsx
@@ -13,56 +13,10 @@ export const loader = async ({ context,params }: LoaderFunctionArgs) => {
       re_str += mergeItems(obj.list) + '\n';
   }
   //return new Response(re_str);
-  up2qiniu(re_str,ids);
-}
-
-function mergeItems(str) {
-    let arr = str.split('\n');
-    // 过滤掉空值
-    let filteredArr = arr.filter(item => item);
-
-    // 我们首先创建一个空对象来存储合并后的结果
-    let merged = {};
-
-    // 遍历数组中的每个字符串
-    filteredArr.forEach(item => {
-        // 将字符串按逗号分割成前缀和后缀
-        let [prefix, suffix] = item.split(',');
-        // 过滤掉空字符串和undefined值
-        if (prefix && suffix) {
-            // 如果前缀已经存在于merged对象中，则将后缀添加到对应的Set中
-            if (merged[prefix]) {
-                merged[prefix].add(suffix);
-            } else {
-                // 如果前缀不存在，则创建一个新的Set来存储后缀
-                merged[prefix] = new Set([suffix]);
-            }
-        }
-    });
-
-    // 现在我们将合并后的结果转换为所需的字符串格式
-    let result = Object.keys(merged).map(key => {
-        return `${key},${Array.from(merged[key]).join('#')}`;
-    }).join('\n'); // 使用换行符将每个合并后的项连接成一个字符串
-
-    return result;
-}
-
-// 示例使用
-//let arr = ['cctv1,xxx1', 'cctv1,xxx2', '', 'cctv2,yyy1', null, 'cctv2,yyy2', 'cctv2,', 'cctv2,yyy2'];
-//console.log(mergeItems(arr)); // 输出: 'cctv1,xxx1#xxx2\ncctv2,yyy1#yyy2'
 
 
-
-
-
-
-
-
-
-
-function up2qiniu(up_str,id){
-  //将上面整理好的代码上传至qiniu
+  
+    //将上面整理好的代码上传至qiniu
   // 替换为你的七牛云 Access Key 和 Secret Key
   const ACCESS_KEY = '_xrjdAPeGkNOy_Kuw_ZK-CarsgrnJbuGMyt5gxZP';
   //return new Response(process.env.QINIU_ACCESS_KEY);
@@ -74,7 +28,7 @@ function up2qiniu(up_str,id){
 
   // 要上传的字符串
   //const stringToUpload = 'Hello, Qiniu Cloud!';
-  const stringToUpload = up_str;
+  const stringToUpload = re_str;
 
   // 构建上传策略
   const policy = {
@@ -114,3 +68,39 @@ function up2qiniu(up_str,id){
   // 返回响应结果
   return response;
 }
+
+function mergeItems(str) {
+    let arr = str.split('\n');
+    // 过滤掉空值
+    let filteredArr = arr.filter(item => item);
+
+    // 我们首先创建一个空对象来存储合并后的结果
+    let merged = {};
+
+    // 遍历数组中的每个字符串
+    filteredArr.forEach(item => {
+        // 将字符串按逗号分割成前缀和后缀
+        let [prefix, suffix] = item.split(',');
+        // 过滤掉空字符串和undefined值
+        if (prefix && suffix) {
+            // 如果前缀已经存在于merged对象中，则将后缀添加到对应的Set中
+            if (merged[prefix]) {
+                merged[prefix].add(suffix);
+            } else {
+                // 如果前缀不存在，则创建一个新的Set来存储后缀
+                merged[prefix] = new Set([suffix]);
+            }
+        }
+    });
+
+    // 现在我们将合并后的结果转换为所需的字符串格式
+    let result = Object.keys(merged).map(key => {
+        return `${key},${Array.from(merged[key]).join('#')}`;
+    }).join('\n'); // 使用换行符将每个合并后的项连接成一个字符串
+
+    return result;
+}
+
+// 示例使用
+//let arr = ['cctv1,xxx1', 'cctv1,xxx2', '', 'cctv2,yyy1', null, 'cctv2,yyy2', 'cctv2,', 'cctv2,yyy2'];
+//console.log(mergeItems(arr)); // 输出: 'cctv1,xxx1#xxx2\ncctv2,yyy1#yyy2'
