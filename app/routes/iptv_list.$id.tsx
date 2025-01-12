c27a0794f4bb099d59ad0d11d09a6d6162e22a01
天仙婆婆 <tqx5201@163.com>
2025-01-12 12:44:31 +0800

    Create iptv_list.$id.tsx

---
+++ app/routes/iptv_list.$id.tsx
@@ -0,0 +1,16 @@
+export const loader = async ({ context,params }: LoaderFunctionArgs) => {
+  return new Response(params);
+  const db = context.DB as D1Database;
+  const { id } = params; 
+  const ids = id.split('.')[0];
+  const { results } = await db
+    .prepare("SELECT * FROM iptv_list where yys = ?")
+    .bind(ids)
+    .all();
+  let re_str = '';
+  for (const obj of results) {
+      re_str += obj.name + ',#genre#\n';
+      re_str += mergeItems(obj.list) + '\n';
+  }
+  return new Response(re_str);
+};
